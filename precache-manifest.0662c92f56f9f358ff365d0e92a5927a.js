c014f0aab64aa7f864256e941eca7533db6c6657
Xotonori <xotonori29011991@gmail.com>
2020-04-30 21:01:38 +0300

    Updates

---
+++ precache-manifest.0662c92f56f9f358ff365d0e92a5927a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e29d3dbb5eea03008d11853c1228184",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "279ec8484933607f7d19",
+    "url": "/react-practice/static/css/main.e508140d.chunk.css"
+  },
+  {
+    "revision": "b8bba57cf8f586df25cc",
+    "url": "/react-practice/static/js/2.880b5754.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-practice/static/js/2.880b5754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "279ec8484933607f7d19",
+    "url": "/react-practice/static/js/main.44c51581.chunk.js"
+  },
+  {
+    "revision": "da9de82e2831db2e0422",
+    "url": "/react-practice/static/js/runtime-main.b2e5fe49.js"
+  },
+  {
+    "revision": "35fcf9dd2a9c81da282a9584d48705da",
+    "url": "/react-practice/static/media/avatar.35fcf9dd.jpeg"
+  }
+]);
